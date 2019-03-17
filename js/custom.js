4ea90f0b2b3fe2ec6a55f77f58fdfa1d4542c439
Rishabh Mishra <rishabh9807mishra@gmail.com>
2019-03-17 20:48:42 +0530

    minor changes
    side scroll design
    custom javascript (for further js code)

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+        //Turning Of Carousel Transition
+        $('.carousel').carousel({
+
+            interval: false
+        })
+        //Script For Contact Us Form
+               
+
+        //Script For Contact Us Form
+
+        //Script For Button Text Toggle
+        function readMoreOne() {
+        var x = document.getElementById("readMoreOne");
+        if (x.innerHTML === "Read More") {
+            x.innerHTML = "Close";
+        } else {
+            x.innerHTML = "Read More";
+            }
+        }
+
+        function readMoreTwo() {
+        var x = document.getElementById("readMoreTwo");
+        if (x.innerHTML === "Read More") {
+            x.innerHTML = "Close";
+        } else {
+            x.innerHTML = "Read More";
+            }
+        }
+
+        function readMoreThree() {
+        var x = document.getElementById("readMoreThree");
+        if (x.innerHTML === "Read More") {
+            x.innerHTML = "Close";
+        } else {
+            x.innerHTML = "Read More";
+            }
+        }
